2c4d12fb1d7783b7edd25f83d50b5f26fa9d5bcf
Jordan <jordanogrinc@gmail.com>
2021-09-12 11:49:05 -0400

    testing API call for temp based on city

--- MobileApps1PA/SecondViewController.swift
+++ MobileApps1PA/SecondViewController.swift
@@ -20,6 +20,10 @@ class SecondViewController: UIViewController {
     
     @IBOutlet weak var resAddr2Text: UILabel!
     
+    @IBOutlet weak var TempDisplay: UILabel!
+    
+    @IBOutlet weak var TempTitle: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -39,17 +43,22 @@ class SecondViewController: UIViewController {
     }
     
     @IBAction func secondbtn(_ sender: Any) {
+        let hapgen = UINotificationFeedbackGenerator()
+        hapgen.notificationOccurred(.success)
         let ref = Database.database().reference()
         ref.child("restaurants/res\(Int.random(in:0..<(self.rescount)))").observeSingleEvent(of: .value, with: {snapshot in guard let value = snapshot.value as? [String: Any] else {
             return}
             let resName = value["name"] as? String ?? ""
             let resAddr1 = value["addr1"] as? String ?? ""
             let resAddr2 = value["addr2"] as? String ?? ""
+            let resCity = value["city"] as? String ?? ""
+            let resState = value["state"] as? String ?? ""
             
             self.resNameText.text = "\(resName)"
             self.resAddr1Text.text = "\(resAddr1)"
             self.resAddr2Text.text = "\(resAddr2)"
-            //print("Value: \(value.keys) and \(String(describing: value["name"]))")
+            self.TempTitle.text = "\(resCity) Current Temperature:"
+            print("TEST : https://api.openweathermap.org/data/2.5/weather?q=\(resCity),\(resState.lowercased()),usa&appid=\(self.apiKey)")
         })
     }
     /*
